c67b39eadcaa35fe5720ab6e15028627f8401782
BlakeNeko <blakeneko@outlook.com>
2025-03-03 11:20:15 +0800

    add function for playing a round

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,6 +1,8 @@
 const allChoices = ['rock', 'paper', 'scissors'];
 let computerScore = 0;
 let humanScore = 0;
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
 
 function getComputerChoice() {
   return allChoices[getRandomInt(0, 2)];
@@ -20,3 +22,39 @@ function getRandomInt(min, max) {
   // 返回一个随机整数，最小值为min，最大值为max。
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+function playRound(computerChoice, humanChoice) {
+  if (computerChoice === 'rock') {
+    if (humanChoice === 'rock') {
+      console.log(`It's a draw! ${computerChoice} and ${humanChoice}.`);
+    } else if (humanChoice === 'paper') {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    }
+  } else if (computerChoice === 'paper') {
+    if (humanChoice === 'rock') {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    } else if (humanChoice === 'paper') {
+      console.log(`It's a draw! ${computerChoice} and ${humanChoice}.`);
+    } else {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    }
+  } else {
+    if (humanChoice === 'rock') {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    } else if (humanChoice === 'paper') {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    } else {
+      console.log(`It's a draw! ${computerChoice} and ${humanChoice}.`);
+    }
+  }
+}
+
+playRound(computerChoice, humanChoice);
