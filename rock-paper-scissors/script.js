d7d669f7cc360368ebabb01340834f222bb87bf4
BlakeNeko <blakeneko@outlook.com>
2025-03-03 10:58:42 +0800

    add functions for getting human and computer choices

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+const allChoices = ['rock', 'paper', 'scissors'];
+let computerScore = 0;
+let humanScore = 0;
+
+function getComputerChoice() {
+  return allChoices[getRandomInt(0, 2)];
+}
+
+function getHumanChoice() {
+  let humanInput = prompt('Please input your choice:');
+  humanInput = humanInput.trim().toLowerCase();
+  if (allChoices.includes(humanInput)) {
+    return humanInput;
+  } else {
+    return null;
+  }
+}
+
+function getRandomInt(min, max) {
+  // 返回一个随机整数，最小值为min，最大值为max。
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
