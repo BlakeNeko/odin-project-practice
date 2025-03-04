015fe0cc7c49c50640458b532f22d8918e44451a
BlakeNeko <blakeneko@outlook.com>
2025-03-04 10:33:55 +0800

    remove unnecessary variables, make playRound function work

--- rock-paper-scissors-ui/script.js
+++ rock-paper-scissors-ui/script.js
@@ -2,8 +2,6 @@ const allChoices = ['rock', 'paper', 'scissors'];
 let computerScore = 0;
 let humanScore = 0;
 let roundCount = 1;
-let humanChoice = undefined;
-let computerChoice = undefined;
 
 // 获取需要操作的DOM元素
 const humanChoiceBox = document.querySelector('.duel .human .choice');
@@ -14,8 +12,6 @@ const buttons = document.querySelectorAll('button');
 const roundWinnerInfo = document.querySelector('.info h2');
 
 function resetGameData() {
-  humanChoice = undefined;
-  computerChoice = undefined;
   computerScore = 0;
   humanScore = 0;
   roundCount = 1;
@@ -41,8 +37,44 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function playRound(computerChoice, humanChoice) {
+  if (computerChoice === 'rock') {
+    if (humanChoice === 'rock') {
+      console.log(`It's a draw! ${computerChoice} and ${humanChoice}.`);
+    } else if (humanChoice === 'paper') {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    }
+  } else if (computerChoice === 'paper') {
+    if (humanChoice === 'rock') {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    } else if (humanChoice === 'paper') {
+      console.log(`It's a draw! ${computerChoice} and ${humanChoice}.`);
+    } else {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    }
+  } else {
+    if (humanChoice === 'rock') {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    } else if (humanChoice === 'paper') {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    } else {
+      console.log(`It's a draw! ${computerChoice} and ${humanChoice}.`);
+    }
+  }
+}
+
 buttons.forEach((button) => {
   button.addEventListener('click', function (event) {
-    console.log(event.target.dataset.choice);
+    let computerChoice = getComputerChoice();
+    let humanChoice = event.target.dataset.choice;
+    playRound(computerChoice, humanChoice);
   });
 });
