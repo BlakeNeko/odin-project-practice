7dc79ba0839bbb6aeecb5396caf3398a88c73dbc
BlakeNeko <blakeneko@outlook.com>
2025-03-04 10:25:02 +0800

    use data attribute to avoid using emoji character in code

--- rock-paper-scissors-ui/script.js
+++ rock-paper-scissors-ui/script.js
@@ -1,20 +1,21 @@
 const allChoices = ['rock', 'paper', 'scissors'];
-const allChoicesEmoji = ['✊', '✋', '✌️'];
 let computerScore = 0;
 let humanScore = 0;
 let roundCount = 1;
+let humanChoice = undefined;
+let computerChoice = undefined;
 
 // 获取需要操作的DOM元素
 const humanChoiceBox = document.querySelector('.duel .human .choice');
 const computerChoiceBox = document.querySelector('.duel .computer .choice');
 const humanScoreInfo = document.querySelector('.duel .human .score');
 const computerScoreInfo = document.querySelector('.duel .computer .score');
-const rockButton = document.querySelector('#rock');
-const paperButton = document.querySelector('#paper');
-const scissorsButton = document.querySelector('#scissors');
+const buttons = document.querySelectorAll('button');
 const roundWinnerInfo = document.querySelector('.info h2');
 
 function resetGameData() {
+  humanChoice = undefined;
+  computerChoice = undefined;
   computerScore = 0;
   humanScore = 0;
   roundCount = 1;
@@ -30,3 +31,9 @@ function resetGameUI() {
 
 resetGameData();
 resetGameUI();
+
+buttons.forEach((button) => {
+  button.addEventListener('click', function (event) {
+    console.log(event.target.dataset.choice);
+  });
+});
