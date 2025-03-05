082d0ff61139f8686c96273f080acbcba7a4ab88
BlakeNeko <blakeneko@outlook.com>
2025-03-05 10:08:08 +0800

    refactor: add createCanvas function

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,17 +1,20 @@
 const canvas = document.querySelector('.canvas');
 
-let canvasSize = 16;
-for (let rowCount = 1; rowCount <= canvasSize; rowCount++) {
-  let row = document.createElement('div');
-  row.className = 'row';
-  for (let gridCount = 1; gridCount <= canvasSize; gridCount++) {
-    let grid = document.createElement('div');
-    grid.className = 'grid';
-    row.appendChild(grid);
+function createCanvas(canvasSize) {
+  for (let rowCount = 1; rowCount <= canvasSize; rowCount++) {
+    let row = document.createElement('div');
+    row.className = 'row';
+    for (let gridCount = 1; gridCount <= canvasSize; gridCount++) {
+      let grid = document.createElement('div');
+      grid.className = 'grid';
+      row.appendChild(grid);
 
-    grid.addEventListener('mouseenter', function () {
-      this.style.backgroundColor = '#000000';
-    });
+      grid.addEventListener('mouseenter', function () {
+        this.style.backgroundColor = '#000000';
+      });
+    }
+    canvas.appendChild(row);
   }
-  canvas.appendChild(row);
 }
+
+createCanvas(8);
