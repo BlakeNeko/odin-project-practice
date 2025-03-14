7a6b69719dfb1dfa3481b8631678c9aca75a5f94
BlakeNeko <blakeneko@outlook.com>
2025-03-14 19:44:11 +0800

    implement check winner, rename variable name from location to position

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -10,16 +10,51 @@ const gameStatus = (function () {
     return Math.random() < 0.5 ? 'X' : 'O';
   }
 
-  function placeChess(location) {
-    if (board[location] !== '') {
+  function placeChess(position) {
+    if (board[position] !== '') {
       alert('You cannot place your chess there!');
     }
 
-    board[location] = currentPlayer;
+    board[position] = currentPlayer;
+  }
+
+  function checkWinner() {
+    let winConditons = [
+      // 行
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // 列
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // 对角线
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let condition of winConditons) {
+      let [pos1, pos2, pos3] = condition;
+      if (
+        board[pos1] !== '' &&
+        board[pos1] === board[pos2] &&
+        board[pos2] === board[pos3]
+      ) {
+        return board[pos1];
+      }
+    }
+
+    if (board.every((each) => each !== '')) {
+      return 'tie';
+    }
+
+    return null;
   }
 
   return {
     resetBoard,
     randomChoosePlayer,
+    placeChess,
+    checkWinner,
   };
 })();
