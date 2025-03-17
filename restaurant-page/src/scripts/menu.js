2d86a3d375101febddff74eb78f9e41aeb8e2ae7
BlakeNeko <blakeneko@outlook.com>
2025-03-17 12:09:43 +0800

    use array and object to store dishes data

--- restaurant-page/src/scripts/menu.js
+++ restaurant-page/src/scripts/menu.js
@@ -1,5 +1,14 @@
 import { content } from '../index.js';
 
+const dishesData = [
+  { name: 'Neapolitan Pizza', price: 10 },
+  { name: 'Cacciatore', price: 30 },
+  { name: 'Pasta', price: 12 },
+  { name: 'Latte', price: 7 },
+  { name: 'Beef Noodles', price: 10 },
+  { name: 'Yangzhou fried rice', price: 12 },
+];
+
 function renderMenuPage() {
   content.innerHTML = '';
   content.className = 'menu';
