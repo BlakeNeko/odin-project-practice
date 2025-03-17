e2380fac5a6afe8aff815c89ad4f07f215bb9bba
BlakeNeko <blakeneko@outlook.com>
2025-03-17 14:55:46 +0800

    add click handler for navigation links

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -4,7 +4,16 @@ import { renderContactPage } from './scripts/contact.js';
 import './style.css';
 
 const content = document.querySelector('#content');
+const homeLink = document.querySelector('#home');
+const menuLink = document.querySelector('#menu');
+const contactLink = document.querySelector('#contact');
 
-renderContactPage();
+document.addEventListener('DOMContentLoaded', () => {
+  renderHomePage();
+
+  homeLink.addEventListener('click', renderHomePage);
+  menuLink.addEventListener('click', renderMenuPage);
+  contactLink.addEventListener('click', renderContactPage);
+});
 
 export { content };
