a6fe2d14ad4c53b5cb5fef9bb40a866b895870a7
BlakeNeko <blakeneko@outlook.com>
2025-03-19 18:18:06 +0800

    split code into multiple files

---
+++ todo-list/data.js
@@ -0,0 +1,31 @@
+let tasks = [
+  {
+    id: '1',
+    title: 'Task 1',
+    description: 'desc 1',
+    dueDate: '2025-03-19',
+    priority: 'low',
+    isDone: false,
+    category: 'Uncategorized',
+  },
+  {
+    id: '2',
+    title: 'Task 2',
+    description: 'desc 2',
+    dueDate: '2025-03-20',
+    priority: 'medium',
+    isDone: false,
+    category: 'Uncategorized',
+  },
+  {
+    id: '3',
+    title: 'Task 3',
+    description: 'desc 3',
+    dueDate: '2025-03-21',
+    priority: 'high',
+    isDone: true,
+    category: 'Uncategorized',
+  },
+];
+
+export { tasks };
