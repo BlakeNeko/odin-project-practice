969df58a3af37cc89e24e6268b36cb9d4d4c2585
BlakeNeko <blakeneko@outlook.com>
2025-03-24 11:44:37 +0800

    fetch weather data

--- weather-app/src/script.js
+++ weather-app/src/script.js
@@ -13,7 +13,34 @@ const humidity = document.getElementById('humidity');
 const windSpeed = document.getElementById('wind-speed');
 const pressure = document.getElementById('pressure');
 
+// API
+let apiKey = ''; // 在此处添加实际的API Key
+let apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`;
+
 function toggleWeatherInfo() {
   emptyState.classList.toggle('hidden');
   weatherInfo.classList.toggle('hidden');
+}
+
+function getWeatherData(cityName) {
+  const params = new URLSearchParams({
+    unitGroup: 'metric',
+    lang: 'zh',
+    key: apiKey,
+  });
+
+  let fullURL = `${apiURL}${cityName}?${params.toString()}`;
+  fetch(fullURL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((jsonData) => {
+      console.log(jsonData);
+    })
+    .catch((error) => {
+      console.log(`Error: ${error}`);
+    });
 }
