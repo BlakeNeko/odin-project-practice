38253044a17cd4328ce457818d30dab438dd3777
BlakeNeko <blakeneko@outlook.com>
2025-03-24 11:06:40 +0800

    get dom elements

---
+++ weather-app/src/script.js
@@ -0,0 +1,14 @@
+// DOM元素
+const cityNameInput = document.getElementById('city-name-input');
+const searchButton = document.getElementById('search-button');
+
+const emptyState = document.querySelector('div.empty-state');
+const weatherInfo = document.querySelector('div.weather-info');
+
+const cityName = document.getElementById('city-name');
+const currentDate = document.getElementById('current-date');
+const temperature = document.getElementById('temperature');
+const weatherText = document.getElementById('weather-text');
+const humidity = document.getElementById('humidity');
+const windSpeed = document.getElementById('wind-speed');
+const pressure = document.getElementById('pressure');
